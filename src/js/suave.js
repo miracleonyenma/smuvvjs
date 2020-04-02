65cc53488491accefbb849f015f905f02b47ab84
miracleonyenma <miracleficient@gmail.com>
2020-04-02 15:02:08 +0100

    update: now initailize suave with object property
    - suaveInit({linkEl, dur, ease}), left empty, default values will be used
    - allows for more dynamic customization

--- src/js/suave.js
+++ src/js/suave.js
@@ -1,21 +1,37 @@
 
 //initialize suave
-function suaveInit(LinkTargetEl, dur, ease){
-    // check if the function parameters have values
-    // if not, assign default values
-    LinkTargetEl == undefined ? LinkTargetEl = document.querySelectorAll(".suaveLink") : LinkTargetEl = document.querySelectorAll(toString(LinkTargetEl))
-    dur == undefined ? dur = 800 : dur = dur;
-    ease == undefined ? ease = easing : ease = ease;
-    
-    for(i = 0; i < LinkTargetEl.length; i++){
-        LinkTargetEl[i].addEventListener("click", function(e){
-            e.preventDefault();
-            smoothScroll(e.target.getAttribute("href"), dur);
-        });
-    };    
-    
-    //suave has been initialized!
-    suaved = true;
+function suaveInit({linkEl, dur, ease}){
+    //run code when the dom is interactive - complete
+    document.addEventListener('readystatechange', function(e){
+        if(e.target.readyState === "interactive"){
+            console.log("init");
+            console.log(e.target.readyState);
+        }
+        
+        if(e.target.readyState === "complete"){
+            suaved ? suaved = true : console.log("Suave Tip: For suave to work on your site, you need to initialize it:\n \n <script> \n   suaveInit(); \n </script>");
+            
+
+
+            console.log(linkEl, dur, ease);
+            // check if the function parameters have values
+            // if not, assign default values
+            linkEl == undefined ? linkEl = document.querySelectorAll(".suaveLink") : linkEl = document.querySelectorAll(linkEl)
+            dur == undefined ? dur = 800 : dur = dur;
+            ease == undefined ? ease = easing : ease = ease;
+            
+            for(i = 0; i < linkEl.length; i++){
+                linkEl[i].addEventListener("click", function(e){
+                    e.preventDefault();
+                    smoothScroll(e.target.getAttribute("href"), dur);
+                });
+            };    
+            console.log(linkEl, dur, ease);
+            //suave has been initialized!
+            suaved = true;
+        
+        }
+    });
 }
 
 //default easing
@@ -43,20 +59,6 @@ function smoothScroll(e, dur){
     
     requestAnimationFrame(animation);
 };
-
-//run code when the dom is interactive - complete
-document.addEventListener('readystatechange', function(e){
-	if(e.target.readyState === "interactive"){
-        console.log("init");
-        console.log(e.target.readyState);
-    }
-    
-	if(e.target.readyState === "complete"){
-        suaved ? suaveInit() : console.log("Suave Tip: For suave to work on your site, you need to initialize it:\n \n <script> \n   suaveInit(); \n </script>");
-        
-	}
-});
-
 
 //boolean for suave to check if it's been initialized 
 let suaved = false;
