824070f86e982ffdf9576ee0bbb48c6d977282b6
miracleonyenma <miracleficient@gmail.com>
2020-04-03 23:33:02 +0100

    update: major update on demo site, syling and func updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,35 @@
+const toggle = (el, eventType, className)=>{
+    el.addEventListener(eventType, ()=>{
+        el.classList.toggle(className);
+    });
+};
+
+
 document.addEventListener("readystatechange", (e)=>{
-    console.log(e);
-    
-})
+    if(e.target.readyState === "interactive"){
+        //Document is accessible now!
+        //Run some code
+	}
+	if(e.target.readyState === "complete"){
+        //Document is ready! 
+        //Knock yourself out
+        const nav = document.querySelector("nav");
+        const docStyles = document.documentElement.style;
+        const headerFx = ()=>{
+            if(window.scrollY >= 60){
+                docStyles.setProperty("--header-title-nav-height", "67px");
+                docStyles.setProperty("--header-title-nav-position", "fixed");
+                docStyles.setProperty("--header-title-nav-z-index", "999");
+                docStyles.setProperty("--headerSideFx-color", "var(--main)");
+            } else{
+                docStyles.setProperty("--headerSideFx-color", "var(--bg)");
+                docStyles.setProperty("--header-title-nav-position", "absolute");
+                docStyles.setProperty("--header-title-nav-z-index", "0");
+                docStyles.setProperty("--header-title-nav-height", "calc(50vh + 60px)");
+            }        
+        }
+
+        toggle(nav, "click", "nav-is-open");
+        window.addEventListener("scroll", headerFx);
+	}
+});
