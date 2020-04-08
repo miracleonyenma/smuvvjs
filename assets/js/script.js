c078adf36da75d262f802ea92f2e7f77d342b4fa
miracleonyenma <miracleficient@gmail.com>
2020-04-08 12:11:32 +0100

    update: copybtn func - able to select text in element and copy to clipboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,8 @@ document.addEventListener("readystatechange", (e)=>{
         //Document is ready! 
         //Knock yourself out
         const nav = document.querySelector("nav"),
-        copyBtn = document.querySelector("button.copy-btn");
+        copyBtn = document.querySelector("button.copy-btn"),
+        alertBox = document.querySelector("#alert");
         const docStyles = document.documentElement.style;
         const headerFx = ()=>{
             if(window.scrollY >= 60){
@@ -29,12 +30,41 @@ document.addEventListener("readystatechange", (e)=>{
                 docStyles.setProperty("--header-title-nav-height", "calc(50vh + 60px)");
             }        
         }
-        const copyFunc = (e, el)=>{
+        const copyFunc = (e)=>{
             let copyTarget = document.querySelector(e.target.getAttribute("data-target"));
             console.log(copyTarget);
+            if(window.getSelection){
+                let range = document.createRange();
+                let selection = window.getSelection();
+                selection.removeAllRanges();
+                range.selectNodeContents(copyTarget);
+                selection.addRange(range);
+                document.execCommand('copy');
+            } else {
+                alert("Unable to copy text");
+            }
+            customAlert.show(`Copied to clipboard`);
+        };
+        // const customAlert = (content)=>{
+        //     alertBox.classList.add("active-alert");
+        //     alertBox.querySelector("span").textContent = content;
+        // };
+        const customAlert = {
+            show : (content)=>{
+                alertBox.classList.add("active-alert");
+                alertBox.querySelector("span").textContent = content;
+            },
+            hide : ()=>{
+                alertBox.classList.remove("active-alert");
+            }
         }
         toggle(nav, "click", "nav-is-open");
         copyBtn.addEventListener("click", copyFunc);
         window.addEventListener("scroll", headerFx);
+        window.addEventListener("click", (e)=>{
+            if(e.target === alertBox){
+                customAlert.hide();
+            }
+        })
 	}
 });
